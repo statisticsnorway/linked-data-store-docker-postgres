c6d666576b83aea97ad00b7032af2e195ab4fc44
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2018-09-27 12:22:41 +0200

    docker-compose and run-script improvements.

--- run-dev.sh
+++ run-dev.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
+mkdir -p conf
+mkdir -p schemas
+
 if [ "$1" == "clean" ]; then
-  echo "Cleaning existing postgres data volumes"
-  docker rm $(docker ps -aq -f "name=postgresdb_1")
-  docker volume rm $(docker volume ls -q | grep pgdata)
+  echo "Cleaning existing associated volumes and data"
+  docker-compose down
+  docker volume rm $(docker volume ls -q -f "name=ldspostgres")
 else
-  echo "Reusing existing data volumes"
+  echo "Reusing existing volumes and data"
 fi
 
 ENV_FILE='docker-compose.env'
 if [ -f $ENV_FILE ]; then
-    export $(grep -v '^#' $ENV_FILE | xargs -0)
+    export $(grep -v '^#' $ENV_FILE | envsubst | xargs -0)
 fi
 
 docker-compose up --remove-orphans
